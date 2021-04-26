f6e33ecf9b1f12f4c3c722e114a73f99b1e719aa
DO506 <12190050.gcit@rub.edu.bt>
2021-04-26 18:25:28 +0600

    TODO_08

---
+++ TOdO_08_ii/app/src/main/java/bt/edu/rub/todo_08_ii/MainActivity.java
@@ -0,0 +1,31 @@
+package bt.edu.rub.todo_08_ii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.net.URI;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView Mmessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Mmessage = findViewById(R.id.TextView);
+
+        Intent obj = getIntent();
+        Uri message = obj.getData();
+
+        if (message != null){
+            String txt = message.toString();
+            Mmessage.setText(txt);
+
+        }
+    }
+}
