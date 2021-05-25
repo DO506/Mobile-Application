05f9d63c1b26ee8a122f21e93442dc0e336828f0
DO506 <12190050.gcit@rub.edu.bt>
2021-05-25 12:54:05 +0600

    AsnycTask

---
+++ TODO_21/app/src/main/java/bt/edu/rub/todo_21/MainActivity.java
@@ -0,0 +1,25 @@
+package bt.edu.rub.todo_21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+   // private static final String TEXT_STATE = "CurrentText";
+    TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textView);
+    }
+
+    public void startTask(View view) {
+        mTextView.setText("Napping......");
+        new SimpleAsyncTask(mTextView).execute();
+
+    }
+}
